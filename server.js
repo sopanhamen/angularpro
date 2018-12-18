8205d380a8ecc07d8b82e524893635f572dcdebf
So_Panha <mensopanha.rupp@gmail.com>
2018-12-18 10:16:26 +0700

    change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/admin-angular'));
+app.use(express.static(__dirname + '/dist/TamplateAdmin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/admin-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/TamplateAdmin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
